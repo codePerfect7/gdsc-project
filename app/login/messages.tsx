3586d68eb4837a9d3e2b957f2d3759f052083a4b
codePerfect7 <89625657+codePerfect7@users.noreply.github.com>
2023-10-10 09:04:18 +0530

    Update messages.tsx

--- app/login/messages.tsx
+++ app/login/messages.tsx
@@ -9,7 +9,7 @@ export default function Messages() {
   return (
     <>
       {error && (
-        <p className="mt-4 p-4 bg-neutral-900 text-neutral-300 text-center">
+        <p className="mt-4 p-4 bg-red-600 text-slate-50 text-center">
           {error}
         </p>
       )}

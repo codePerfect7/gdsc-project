ff6e577b93b88779ef75493f0be60fe8693e0d45
Ayush Thakur <testmailforyt12@gmail.com>
2023-10-12 10:13:15 +0530

    Fixed

--- components/CreatePost.tsx
+++ components/CreatePost.tsx
@@ -3,11 +3,11 @@ import { cookies } from "next/headers"
 
 export const dynamic = 'force-dynamic'
 
-const CreatePost = async ({ title, description }: { title: string | null, description: string | null }) => {
+const CreatePost = async ({ title, description }: { title: string, description: string }) => {
     const supabase = createServerComponentClient<Database>({cookies})
     const {data, error} = await supabase.from('posts').insert([{
         title: title,
-        description: description,
+        content: description,
     }]).select()
     if (error) return {
         error: true,
